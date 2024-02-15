c0707a6419f5ce84ea3896a59309a4b5cec3e172
jeongyunnim <jeongyun7521@gmail.com>
2024-02-15 19:21:59 +0900

    [Feat] 예제 페이지 작성
    로그인을 테스트 해볼 수 있도록 로그인 페이지를 form을 통해서 입력하는
    레이아웃만 구성을 했다.

--- srcs/frontend/tools/srcs/static/assets/scripts/app.js
+++ srcs/frontend/tools/srcs/static/assets/scripts/app.js
@@ -1,6 +1,40 @@
+
+const signin = document.createElement('main');
+signin.className = 'form-signin w-100 m-auto';
+
+const login = document.createElement('form');
+
+const idDiv = document.createElement('div');
+idDiv.className = 'form-floating';
+
+const idInput = document.createElement('input');
+idInput.className = 'form-control';
+idInput.setAttribute('type', 'email');
+
+const pwDiv = document.createElement('div');
+pwDiv.className = 'form-floating';
+
+const pwInput = document.createElement('input');
+pwInput.className = 'form-control';
+pwInput.setAttribute('type', 'float');
+
+const submitButton = document.createElement('button');
+submitButton.className = 'btn btn-primary w-100 py-2'
+submitButton.setAttribute('type', 'submit')
+submitButton.textContent = 'Sign in'
+
 class app {
 	static init() {
-		alert('test');
+		const body = document.querySelector('body');
+		body.className = 'd-flex align-items-center py-4 bg-body-tertiary';
+		const application = document.querySelector('#app');
+		idDiv.append(idInput);
+		login.append(idDiv);
+		pwDiv.append(pwInput);
+		login.append(pwDiv);
+		login.append(submitButton);
+		signin.append(login);
+		application.append(signin);
 	}
 }
 
